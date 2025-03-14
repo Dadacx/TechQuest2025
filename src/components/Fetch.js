a1f5cdc7ffd5adac6fc1b920a229eec22b52af45
Dadacx <84072588+Dadacx@users.noreply.github.com>
2025-03-14 19:10:36 +0100

    wszystko

---
+++ src/components/Fetch.js
@@ -0,0 +1,17 @@
+import token from '../.token.js'
+const PromptFetch = async (prompt) => {
+    const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${token}`, {
+        method: "POST",
+        body: 
+            `{
+                "contents": [{
+                    "parts":[{"text": "${prompt}"}]
+                }]
+            }`
+    })
+    const json = await res.json()
+
+    return json.candidates[0].content.parts[0].text
+}
+
+export default PromptFetch
