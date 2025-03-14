27f100850d6c57c4d66db0dfaf799d9843746c3e
patrykduda2005 <patrykduda7@gmail.com>
2025-03-14 21:16:20 +0100

    wmiare nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './styles/main.css';
 import Chat from './components/Chat.js';
+import Nav from './components/Nav.js';
 
-import testCharacter from './characters.json'
 
 function App() {
-  const characters = testCharacter
   return (
     <div className="container">
-      <Chat character={characters[0]} />
+      <Nav/>
     </div>
   );
 }
